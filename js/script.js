304d93d7c190a26da21df7c6b4cca3631fe7277c
Moonlie <rockmoonlie@gmail.com>
2023-12-26 19:16:33 +0100

    responsive en cours

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*home with the tables
+first let's make the checkboxes work */
+
+function checkAll() {
+    let select_all = document.getElementById('select_deselect');
+    if (select_all.checked == true){
+        let selectOption = document.getElementsByName ('option')
+        for( let i = 0; i<selectOption.length; i++){
+            selectOption[i].checked = true
+        } 
+    }else {
+        let selectOption = document.getElementsByName ('option')
+        for( let i = 0; i<selectOption.length; i++){
+            selectOption[i].checked = false
+        } 
+    }
+    
+}
